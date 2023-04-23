f8b7898e99f3a4225686a81ca802e75d52ff77e3
lanbinshijie <huoziye123@163.com>
2023-04-23 18:17:47 +0800

    docs: 日常学习NestJS 1

--- chatby-backend/src/app.module.ts
+++ chatby-backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GirlModule } from './girl/girl.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GirlModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
