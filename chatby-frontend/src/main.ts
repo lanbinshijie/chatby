3f37ba5fffbac5bace56832e79e8255635d6cd5b
lanbinshijie <huoziye123@163.com>
2023-04-29 22:31:25 +0800

    chore: 增加注释&pinia的依赖

--- chatby-frontend/src/main.ts
+++ chatby-frontend/src/main.ts
@@ -1,11 +1,21 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router/main'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css'
 
+import { message } from 'ant-design-vue';
+
 const app = createApp(App)
-app.use(router)
-app.use(Antd)
+const pinia = createPinia()
+
+// 配置全局属性，调用方法：this.$message.success('成功提示');
+app.config.globalProperties.$message = message;
+
+app.use(router) // 使用路由
+app.use(Antd) // 使用Antd
+app.use(pinia) // 使用pinia
+
 app.mount('#app')
