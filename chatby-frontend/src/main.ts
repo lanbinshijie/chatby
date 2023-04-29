c07c863d412226f81b72566fc73b83197746fc2a
lanbinshijie <huoziye123@163.com>
2023-04-29 12:28:40 +0800

    style: 删除不必要的代码

--- chatby-frontend/src/main.ts
+++ chatby-frontend/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router/main'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(Antd)
+app.mount('#app')
