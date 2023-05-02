e5f188ac55d3f90b00586a2587d156eb5eaccef7
lanbinshijie <huoziye123@163.com>
2023-05-02 14:21:31 +0800

    feat: 后端 完成了后端登录模块

--- chatby-backend/src/modules/auth/auth.controller.ts
+++ chatby-backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RCode } from 'src/common/constants/rcode';
+import { AuthLoginDto, AuthRegisteryDto } from '../dtos/auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -19,26 +20,24 @@ export class AuthController {
         };
     }
 
-    @Get('/register')
-    async register(@Query() query) {
-        // 获取参数username和password
-        const { username, password } = query;
+    @Post('/register')
+    async register(@Body() body: AuthRegisteryDto) {
         // 调用service中的register方法
-        let data = await this.authService.register(username, password);
+        let data = await this.authService.register(body);
         // 根据返回的code值，返回不同的状态码
         let code = data.code === RCode.OK ? 200 : 400;
         let msg = data.code === RCode.OK ? '注册成功！' : '注册失败！' + data.data;
         return {
             code: code,
-            data: data.code === RCode.OK ? data.data : null,
+            data: data.code === RCode.OK ? data.data : "",
             message: msg
         };
     }
 
-    @Get('/login')
-    async login(@Query() query) {
+    @Post('/login')
+    async login(@Body() body: AuthLoginDto) {
         // 获取参数username和password
-        const { username, password, device } = query;
+        const { username, password, device } = body;
         // 调用service中的login方法
         let data = await this.authService.login(username, password, device, false);
         // 根据返回的code值，返回不同的状态码
@@ -46,15 +45,41 @@ export class AuthController {
         let msg = data.code === RCode.OK ? '登录成功！' : '登录失败！' + data.data;
         return {
             code: code,
-            data: data.code === RCode.OK ? data.data : null,
+            data: data.code === RCode.OK ? data.data : "",
             message: msg
         };
     }
 
+    @Post('/loginByToken')
+    async loginByToken(@Body() body: AuthLoginDto) {
+        // 获取参数username和token
+        const { username, password, device } = body;
+        // 调用service中的login方法
+        let data = await this.authService.login(username, password, device, true, true);
+        // 根据返回的code值，返回不同的状态码
+        let code = data.code === RCode.OK ? 200 : 400;
+        let msg = data.code === RCode.OK ? '登录成功！' : '登录失败！' + data.data;
+        return {
+            code: code,
+            data: data.code === RCode.OK ? data.data : "",
+            message: msg
+        };
+    }
 
     @Get('/selectAllUser')
     selectAllUser() {
         return this.authService.selectAllUser();
+    }
+
+    // 获取请求者的IP地址
+    @Get('/ip')
+    getIp(@Request() request) {
+
+        return {
+            code: 200,
+            ip: request.headers['x-forwarded-for'] || request.connection.remoteAddress,
+            message: '获取IP地址成功！'
+        }
     }
 
 }
--- chatby-backend/src/modules/auth/auth.service.ts
+++ chatby-backend/src/modules/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Repository, Like } from 'typeorm'
+import { Repository, Like, MoreThan } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Auth } from './entities/auth.entities';
 import { AuthToken } from './entities/authToken.entities';
 import { RCode } from 'src/common/constants/rcode';
 import { nameVerify } from 'src/common/tools/tools';
+import { login_afterSuccess } from './auth.extra';
+import { AuthRegisteryDto, DeviceDto } from '../dtos/auth.dto';
 
 
 
@@ -17,40 +19,33 @@ export class AuthService {
     ) {}
     
 
-    async register(username, password) {
-        if(nameVerify(username) && nameVerify(password)) { // 验证用户名和密码是否合法
-            // 查询是否已经存在该用户
-            let user_name = await this.authRepository.findOne({where: {username: username}});
-            console.log(username, password, user_name);
-            if(user_name) { // 如果存在，返回错误信息
-                return {
-                    code: RCode.ERROR,
-                    data: "用户名重复！"
-                }
-            }
-
-            const data = new Auth();
-            data.username = username;
-            data.password = password;
-            data.nickname = username;
-            data.avatar = 'https://avatars.githubusercontent.com/u/62149873?v=4';
-            data.email = username+"@chatby.top";
-            data.isBanned = false;
-            data.isVerifyMail = false;
-            data.role = 'user';
-            data.lastLoginTime = new Date();
-            // Save
-            this.authRepository.save(data);
-            return {
-                code: RCode.OK,
-                data: data
-            }
-
-        } else {
+    async register(user: AuthRegisteryDto) {
+        // 查询是否已经存在该用户
+        let user_name = await this.authRepository.findOne({
+            where: [{username: user.username}, {email: user.email}],
+        });
+        if(user_name) { // 如果存在，返回错误信息
             return {
                 code: RCode.ERROR,
-                data: "用户名或密码不合法！"
+                data: "用户名或邮箱重复！"
             }
+        }
+
+        const data = new Auth();
+        data.username = user.username;
+        data.password = user.password;
+        data.nickname = user.nickname;
+        data.avatar = 'https://avatars.githubusercontent.com/u/62149873?v=4';
+        data.email = user.email;
+        data.isBanned = false;
+        data.isVerifyMail = false;
+        data.role = 'user';
+        data.lastLoginTime = new Date();
+        // Save
+        this.authRepository.save(data);
+        return {
+            code: RCode.OK,
+            data: data
         }
     }
     
@@ -60,9 +55,17 @@ export class AuthService {
         return r;
     }
 
-    private async createToken(user: Auth, device: string) {
+    private async createToken(user: Auth, device: DeviceDto) {
         // 查找是否已经有Token，如果有Device相同的，就更新Token，否则就创建新的Token
-        let token = await this.authTokenRepository.findOne({where: {uid: user.id, deviceId: device}});
+        // 还要注意Token不能过期，过期也无效
+        let token = await this.authTokenRepository.findOne({
+                where: {
+                    uid: user.id, 
+                    deviceId: device.deviceId,
+                    // 时间没有过期（expire > now），Expire是一个Date
+                    expireTime: MoreThan(new Date())
+                }
+            });
         if(token) {
             token.token = this.createRandomToken();
             token.lastLoginTime = new Date();
@@ -70,51 +73,74 @@ export class AuthService {
         } else {
             token = new AuthToken();
             token.uid = user.id;
-            token.deviceId = device;
+            token.deviceId = device.deviceId;
+            token.deviceType = device.deviceType,
+            token.ip = device.ip,
             token.token = this.createRandomToken();
             token.lastLoginTime = new Date();
+            // 过期时间是一个月
+            token.expireTime = new Date(new Date().getTime() + 30*24*60*60*1000);
             this.authTokenRepository.save(token);
         }
         return token;
     }
 
-    async login(username, password, device: string, remember: boolean) {
-        if(nameVerify(username) && nameVerify(password) && nameVerify(device)) { // 验证用户名和密码是否合法
-            // 用户名登录（用户名和密码都是一样的）
-            let user_name = await this.authRepository.findOne({where: {username: username, password: password}});
-            if(user_name) { // 如果存在，返回用户信息
-                
+    async login(username, password, device: DeviceDto, remember: boolean, token: boolean = false) {
+        if(token) {
+            // 如果是Token登录，那么就直接返回Token
+            let token = await this.authTokenRepository.findOne({
+                where: {
+                    token: password,
+                    // 时间没有过期（expire > now），Expire是一个Date
+                    expireTime: MoreThan(new Date())
+                }
+            });
+            if(token) {
                 return {
                     code: RCode.OK,
                     data: {
-                        token: await this.createToken(user_name, device),
-                        user: user_name
+                        token: token,
+                        user: await this.authRepository.findOne({where: {id: token.uid}})
                     }
                 }
             }
-            // 邮箱登录
-            let user_email = await this.authRepository.findOne({where: {email: username, password: password}});
-            if(user_email) { // 如果存在，返回用户信息
-                return {
-                    code: RCode.OK,
-                    data: {
-                        token: await this.createToken(user_name, device),
-                        user: user_name
-                    }
-                }
-            }
-            // 否则返回错误信息
             return {
                 code: RCode.ERROR,
-                data: "用户名或密码错误！"
+                data: "Token无效！"
             }
         }
+        
+        let user_name = await this.authRepository.findOne({where: {username: username, password: password}});
+        if(user_name) { // 如果存在，返回用户信息
+            await login_afterSuccess()
+            return {
+                code: RCode.OK,
+                data: {
+                    token: await this.createToken(user_name, device),
+                    user: user_name
+                }
+            }
+        }
+        // 邮箱登录
+        let user_email = await this.authRepository.findOne({where: {email: username, password: password}});
+        if(user_email) { // 如果存在，返回用户信息
+            return {
+                code: RCode.OK,
+                data: {
+                    token: await this.createToken(user_name, device),
+                    user: user_name
+                }
+            }
+        }
+        // 否则返回错误信息
         return {
             code: RCode.ERROR,
-            data: "用户名、密码或格式不合法！"
+            data: "用户名或密码错误！"
         }
     }
     selectAllUser() {
         return this.authRepository.find();
     }
+
+
 }
