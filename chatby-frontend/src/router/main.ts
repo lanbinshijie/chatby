571c8cb8a02a6b530315ab9a20035c08267d35a7
lanbinshijie <huoziye123@163.com>
2023-05-02 14:23:19 +0800

    chore: 适配了路由

--- chatby-frontend/src/router/main.ts
+++ chatby-frontend/src/router/main.ts
@@ -7,6 +7,9 @@ const ChatPage = () => import('../pages/chat.vue')
 const routes = [
     { path: '/', redirect: '/chat' },
     { path: '/chat', component: ChatPage },
+    { path: '/login', component: () => import('../pages/login.vue') },
+    // 其余路由重定向到404
+    // { path: '/:pathMatch(.*)*', redirect: '/404' },
 ]
 
 const router = createRouter({
