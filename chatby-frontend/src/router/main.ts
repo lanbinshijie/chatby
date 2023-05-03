8eaff3d6e46e8d11b757b08373c77ef3c4b88a84
lanbinshijie <huoziye123@163.com>
2023-05-03 21:41:32 +0800

    style: router组件导入一致性

--- chatby-frontend/src/router/main.ts
+++ chatby-frontend/src/router/main.ts
@@ -2,11 +2,10 @@
 // Vue Router
 import { createRouter, createWebHistory } from 'vue-router'
 
-const ChatPage = () => import('../pages/chat.vue')
 
 const routes = [
     { path: '/', redirect: '/chat' },
-    { path: '/chat', component: ChatPage },
+    { path: '/chat', component: () => import('../pages/chat.vue') },
     { path: '/login', component: () => import('../pages/login.vue') },
     // 其余路由重定向到404
     // { path: '/:pathMatch(.*)*', redirect: '/404' },
