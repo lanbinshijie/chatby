251dadf41058aa0bf179c1963e97ebad819819d8
lanbinshijie <huoziye123@163.com>
2023-05-03 21:46:25 +0800

    feat: 后端好友聊天增加三个接口：获取所有聊天、获取指定好友聊天、发送消息的接口

--- chatby-backend/src/modules/chat/chat.controller.ts
+++ chatby-backend/src/modules/chat/chat.controller.ts
@@ -1,17 +1,46 @@
 
-import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request } from '@nestjs/common';
 import { RCode } from 'src/common/constants/rcode';
 import { AuthLoginDto, AuthRegisteryDto } from '../dtos/auth.dto';
 import { ChatService } from './chat.service';
+import { FriendMessageDto } from '../dtos/chat.dto';
+import { AuthService } from '../auth/auth.service';
 
 @Controller('chat')
 export class ChatController {
 
-    constructor(private readonly chatService: ChatService) {}
+    constructor(
+        private readonly chatService: ChatService
+    ) {}
 
     @Get("/friend/getallfms")
-    async getAllFriendMessages() {
+    async getAllFriendMessages(@Request() req) {
+        const verifyReqHeader = await this.chatService.verifyReqHeader(req);
+        if (verifyReqHeader.code !== 200) {
+            return verifyReqHeader;
+        }
         const data = await this.chatService.getAllFriendMessages()
+        return data;
+    }
+
+    // /chat/friend/getfriendmessage
+    @Get("/friend/getfriendmessage")
+    async getFriendMessage(@Request() req, @Body() body: any) {
+        const verifyReqHeader = await this.chatService.verifyReqHeader(req);
+        if (verifyReqHeader.code !== 200) {
+            return verifyReqHeader;
+        }
+        const data = await this.chatService.getFriendMessage(req.headers.token, body.friendId)
+        return data;
+    }
+
+    @Post("/friend/sendmessage")
+    async sendMessage(@Request() req, @Body() body: any) {
+        const verifyReqHeader = await this.chatService.verifyReqHeader(req);
+        if (verifyReqHeader.code !== 200) {
+            return verifyReqHeader;
+        }
+        const data = await this.chatService.sendMessage(body.message, req.headers.token)
         return data;
     }
     
--- chatby-backend/src/modules/chat/chat.module.ts
+++ chatby-backend/src/modules/chat/chat.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { FriendMessages } from './entities/chat.entities';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { TokenGuardMiddleware } from 'src/common/middleware/token-guard/token-guard.middleware';
+import { AuthToken } from '../auth/entities/authToken.entities';
+import { AuthService } from '../auth/auth.service';
+import { Auth } from '../auth/entities/auth.entities';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FriendMessages])],
+  imports: [TypeOrmModule.forFeature([FriendMessages, AuthToken, Auth])],
   controllers: [ChatController],
-  providers: [ChatService]
+  providers: [ChatService, AuthService]
 })
-export class ChatModule {}
+export class ChatModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(TokenGuardMiddleware).forRoutes('chat')
+  }
+}
